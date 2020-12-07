30f478b44dc80d633a5daf1a0e8442fbe3b117e7
Panda15x <chayanmandal154@gmail.com>
2020-12-07 15:03:43 +0530

    creates upload directory if it doesn't exist

--- videoFileManager.js
+++ videoFileManager.js
@@ -3,6 +3,10 @@ const path = require('path');
 
 const dirPath = path.join(__dirname, 'public', 'videos');
 
+if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+}
+
 function videoFiles() {
     const files = fs.readdirSync(dirPath);
     files.forEach((item, index, arr) => {
@@ -19,6 +23,9 @@ function fileNames() {
     return files;
 }
 
-const videoData = { videos: videoFiles(), names: fileNames() };
+function videoData() {
+    const vidData = { videos: videoFiles(), names: fileNames() };
+    return vidData;
+}
 
 module.exports = { videoFiles, fileNames, dirPath, videoData };
